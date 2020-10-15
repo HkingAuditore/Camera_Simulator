23cdef9b494239f8c7846ef9ac6de896eb769bd7
HkingAuditore <178854663@qq.com>
2020-10-15 11:26:52 +0800

    Init

---
+++ Assets/Scripts/Camera/LensControllor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LensControllor : MonoBehaviour {
+
+    public GameObject lenTop, lenMid;
+    public Camera camera;
+    public int fOVmin, fOVmax,fOVmid;
+
+	void Update () {
+		if(camera.fieldOfView > fOVmid)
+        {
+            lenMid.transform.localPosition = new Vector3(0, 0, 0.1f + (camera.fieldOfView-fOVmid)/(fOVmax-fOVmid)*(1.37f-0.1f));
+        }
+        else
+        {
+            lenTop.transform.localPosition = new Vector3(0,  -0.03f - (camera.fieldOfView - fOVmin) / (fOVmid - fOVmin) *(0.77f - 0.03f),0);
+        }
+	}
+}
---
+++ Assets/Scripts/Camera/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject[] compositionLine = new GameObject[6];
+
+    private int switchNum = 0;
+    public void NextOne()
+    {
+        compositionLine[switchNum++].SetActive(false);
+        if (switchNum == 6)
+        {
+            switchNum = 0;
+        }
+        compositionLine[switchNum].SetActive(true);
+    }
+
+    public void LastOne()
+    {
+        compositionLine[switchNum--].SetActive(false);
+        if (switchNum == -1)
+        {
+            switchNum = 5;
+        }
+        compositionLine[switchNum].SetActive(true);
+    }
+}
